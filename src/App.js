bf40111c3932cc072379fde13623568fb1c76c7b
ariffikrie <ariffikrie99@gmail.com>
2024-12-22 16:21:30 +0900

    reset

--- src/App.js
+++ src/App.js
@@ -1,27 +1,3 @@
-import logo from './logo.svg';
-import './style/App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
 const sec = document.querySelectorAll('section');
 const links = document.querySelectorAll('header nav a');
 
@@ -40,5 +16,3 @@ window.onscroll = () => {
         }
     });
 }
-
-export default App;
